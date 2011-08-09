8e30cf4c54225438c791bf356a6d4f99219a14e1
Christian Trabold <christian.trabold@dkd.de>
2011-08-09 09:58:26 +0200

    Initial import

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+maintainer       "dkd Internet Service GmbH"
+maintainer_email "christian.trabold@dkd.de"
+license          "All rights reserved"
+description      "Installs/Configures redis"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+recipe            "redis", "Includes the package recipe by default."
+recipe            "redis::package", "Sets up a redis server."
+recipe            "redis::gem", "Installs redis gem for ruby development."
+recipe            "redis::source", "Builds redis server from sources. Not yet implemented."
+recipe            "redis::remove", "Removes redis server and redis gem, if installed."
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
