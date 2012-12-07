027c9eb922a5c98e01b8687be1697fe0016ccdeb
Dmytro Kovalov <dmytro.kovalov@gmail.com>
2012-12-07 12:03:34 +0900

    change default to bind on non local ip

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-default['redis']['bind']         = "127.0.0.1"
+default['redis']['bind']         = node['ipaddress']
 default['redis']['port']         = "6379"
 default['redis']['config_path']  = "/etc/redis/redis.conf"
 default['redis']['daemonize']    = "no"
