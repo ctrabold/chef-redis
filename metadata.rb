676488ecab4afae73b45f598e8c9344610980a4a
Christian Trabold <info@christian-trabold.de>
2013-02-09 00:39:44 +0100

    Add 'name' attribute & fix whitespace

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,12 @@ license          "All rights reserved"
 description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.4"
-recipe            "redis", "Includes the package recipe by default."
-recipe            "redis::package", "Sets up a redis server."
-recipe            "redis::gem", "Installs redis gem for ruby development."
-recipe            "redis::source", "Builds redis server from sources."
-recipe            "redis::remove", "Removes redis server and redis gem, if installed."
+name             "redis"
+recipe           "redis", "Includes the package recipe by default."
+recipe           "redis::package", "Sets up a redis server."
+recipe           "redis::gem", "Installs redis gem for ruby development."
+recipe           "redis::source", "Builds redis server from sources."
+recipe           "redis::remove", "Removes redis server and redis gem, if installed."
 
 %w{ ubuntu debian }.each do |os|
   supports os
